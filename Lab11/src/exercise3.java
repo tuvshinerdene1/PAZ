c8e50ad784b555ec5c06dc5fc4ee1a6c009b3914
tuvshin.erdene25@gmail.com <tuvshin.erdene25@gmail.com>
2024-12-17 16:44:21 +0800

    lab 11 some problems

---
+++ Lab11/src/exercise3.java
@@ -0,0 +1,16 @@
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
+
+public class exercise3 extends GraphicsProgram {
+	
+	RandomGenerator rgen = RandomGenerator.getInstance();
+	
+	public void run(){
+
+	}
+	
+	public static void main(String[] args){
+		new exercise3().start();
+	}
+}
